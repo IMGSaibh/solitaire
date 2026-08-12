492b99cf36d2dcdab3ce6a44dee0fde957f1a809
IMGSaibh <mathiasgewissen@gmx.de>
2026-08-12 23:59:00 +0200

    resized cards

--- scripts/resources/solitaire_card_theme.gd
+++ scripts/resources/solitaire_card_theme.gd
@@ -3,7 +3,7 @@ extends Resource
 
 @export var face_directory := "res://assets/cards/faces"
 @export var back_texture: Texture2D
-@export var card_size := Vector2(150.0, 200.0)
+@export var card_size := Vector2(150.0, 210.0)
 @export var tableau_offset := 50.0
 @export var outline_color := Color.RED
 @export_range(0.0, 3.0) var outline_width := 2.0
