8d8eda248bcee893d25589adf6cb18a7e4940cbb
IMGSaibh <mathiasgewissen@gmx.de>
2026-08-30 16:18:40 +0200

    refactor

--- scripts/views/pile_view.gd
+++ scripts/views/pile_view.gd
@@ -87,7 +87,9 @@ func _adopt_card_view(card_view: CardView) -> bool:
 		_connect_card_view(card_view)
 		return false
 	if old_pile_view != null:
-		old_pile_view.release_card_view(card_view)
+		# old_pile_view.release_card_view(card_view)
+		card_views.erase(card_view)
+		_disconnect_card_view_signals(card_view)
 	card_view.reparent(self, true)
 	_connect_card_view(card_view)
 	return true
@@ -101,10 +103,7 @@ func _connect_card_view(card_view: CardView) -> void:
 	if not card_view.drag_finished.is_connected(_on_card_drag_finished):
 		card_view.drag_finished.connect(_on_card_drag_finished)
 
-
-func release_card_view(card_view: CardView) -> void:
-	card_views.erase(card_view)
-	_disconnect_card_view_signals(card_view)
+# func release_card_view(card_view: CardView) -> void:
 
 
 func _disconnect_card_view_signals(card_view: CardView) -> void:
